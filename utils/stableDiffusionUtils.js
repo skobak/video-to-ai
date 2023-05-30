3be97629abc76d75c5eff6eedd7e63f42fbf2a30
Alexander Skobeltsyn <skob@Alex-Mac.local>
2023-05-30 15:56:43 +0200

    documentation: add chatGPT generated comments on code base

--- utils/stableDiffusionUtils.js
+++ utils/stableDiffusionUtils.js
@@ -1,11 +1,15 @@
 import { WebSocket } from 'ws'
 
+// WebSocket server URL
 const ws_url = 'ws://127.0.0.1:7860/queue/join'
+
+// Static initiation command for the WebSocket server
 const staticInitiationCommand = {
   fn_index: 162,
   session_hash: '8yzyqk2gv88',
 }
 
+// Log the prompt and its associated properties
 const logPrompt = (payload) => {
   console.log(
     `Prompt: ${payload.data[2]},
@@ -15,9 +19,11 @@ const logPrompt = (payload) => {
   )
 }
 
+// Send the payload to the WebSocket server and return the created image file name
 const generateAIImage = (payload) => {
   return new Promise((resolve, reject) => {
     const ws = new WebSocket(ws_url)
+
     ws.on('message', (event) => {
       try {
         const result = JSON.parse(event)
@@ -27,13 +33,16 @@ const generateAIImage = (payload) => {
 
         switch (msg) {
           case 'send_hash':
+            // The server asks for the initiation command, send it
             ws.send(JSON.stringify(staticInitiationCommand))
             break
           case 'send_data':
+            // The server asks for the payload, log the payload and send it
             logPrompt(payload)
             ws.send(JSON.stringify(payload))
             break
           case 'process_completed':
+            // The server notifies that the process is completed, log the message and resolve the promise with the created file name
             console.log('Done')
             const createdFileName = result['output']['data'][0][0]['name']
             ws.removeAllListeners()
@@ -42,6 +51,7 @@ const generateAIImage = (payload) => {
             break
         }
       } catch (e) {
+        // If an error occurs, log it and reject the promise
         console.log(e)
         reject(e)
       }
